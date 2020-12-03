c6f152f8b84718337bf805ce380eb80af6d33df2
Ehsan <ehsan.h89@gmail.com>
2020-12-03 13:52:24 -0800

    all files

---
+++ 1.py
@@ -0,0 +1,8 @@
+#this is my first python program 
+#Defining a list of name 
+names=["Ehsan","Rahim","Ali", "Waheed"]
+print (names)
+names.append("Tahmin")
+print(names)
+names.sort()
+print(names)
